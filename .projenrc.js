951d0252f5f2b94eb2d1bd4baf0b099df5007c6e
Mischa Spiegelmock <me@mish.dev>
2022-09-30 17:34:20 -0700

    config

--- .projenrc.js
+++ .projenrc.js
@@ -3,13 +3,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Mischa Spiegelmock',
   authorAddress: 'me@mish.dev',
   cdkVersion: '2.39.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-nextjs',
+  defaultReleaseBranch: 'master',
+  name: 'cdk-nextjs-standalone',
   repositoryUrl: 'https://github.com/jetbridge/nextjs-cdk.git',
   authorOrganization: 'JetBridge',
   packageName: 'cdk-nextjs-standalone',
   description: 'Deploy a NextJS app to AWS using CDK. Uses standalone build and output tracing.',
-  keywords: ['nextjs', 'aws', 'cdk', 'standalone', 'next'],
+  keywords: ['nextjs', 'next', 'aws', 'cdk', 'standalone', 'iac', 'infrastructure', 'cloud', 'serverless'],
+  eslintOptions: { prettier: true, ignorePatterns: ['assets/**/*'] },
 
   bundledDeps: [
     'cross-spawn',
@@ -23,7 +24,5 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'aws-lambda',
   ] /* Runtime dependencies of this module. */,
   // devDeps: [],             /* Build dependencies for this module. */
-  eslintOptions: { prettier: true, ignorePatterns: ['assets/**/*'] },
-  // bundlerOptions: { assetsDir: 'assets' },
 });
 project.synth();

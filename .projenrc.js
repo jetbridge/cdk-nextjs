094d3b9f88ce2840c4e232319d34fe923f22356f
Mischa Spiegelmock <me@mish.dev>
2022-12-14 16:34:20 -0800

    aws-cdk-lib 2.50.0

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'JetBridge',
   authorAddress: 'mischa@jetbridge.com',
-  cdkVersion: '2.53.0',
+  cdkVersion: '2.50.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-nextjs-standalone',
   repositoryUrl: 'https://github.com/jetbridge/cdk-nextjs.git',

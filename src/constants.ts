ff9da82a44012149cacc7cd0c8ec631d1d983090
Kevin Mitchell <>
2023-07-29 13:30:21 -0400

    chore: add comment for reason for default memory size

--- src/constants.ts
+++ src/constants.ts
@@ -6,4 +6,8 @@ export const DEFAULT_STATIC_STALE_WHILE_REVALIDATE = Duration.days(1).toSeconds(
 
 export const LAMBDA_RUNTIME = Runtime.NODEJS_18_X;
 
+/**
+ * 1536mb costs 1.5x but runs twice as fast for most scenarios.
+ * @see {@link https://dev.to/dashbird/4-tips-for-aws-lambda-optimization-for-production-3if1}
+ */
 export const DEFAULT_LAMBA_MEMORY = 1536;

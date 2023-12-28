3f1ffafc2e347eb626f3b2effef2f2d77b45de9e
Ben Stickley <bestickley@gmail.com>
2023-12-28 10:30:13 -0500

    fix: remove symlinks for pnpm windows bundle

--- src/utils/create-archive.ts
+++ src/utils/create-archive.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process';
-import * as fs from 'node:fs';
-import * as os from 'node:os';
-import * as path from 'node:path';
+import { Dirent, cpSync, existsSync, mkdtempSync, readdirSync, renameSync, rmSync, unlinkSync } from 'node:fs';
+import { tmpdir } from 'node:os';
+import { join } from 'node:path';
 
 export interface CreateArchiveArgs {
   readonly directory: string;
@@ -17,18 +17,24 @@ export interface CreateArchiveArgs {
  * for PNPM monorepos. See more here: https://github.com/aws/aws-cdk/issues/9251
  */
 export function createArchive({ directory, zipFileName, fileGlob = '.', quiet }: CreateArchiveArgs): string {
-  const zipOutDir = fs.mkdtempSync(path.join(os.tmpdir(), 'cdk-nextjs-archive-'));
-  const zipFilePath = path.join(zipOutDir, zipFileName);
+  const zipOutDir = mkdtempSync(join(tmpdir(), 'cdk-nextjs-archive-'));
+  const zipFilePath = join(zipOutDir, zipFileName);
 
-  // delete existing zip file
-  if (fs.existsSync(zipFilePath)) {
-    fs.unlinkSync(zipFilePath);
+  if (existsSync(zipFilePath)) {
+    // delete existing zip file
+    unlinkSync(zipFilePath);
   }
 
-  // run script to create zipfile, preserving symlinks for node_modules (e.g. pnpm structure)
   const isWindows = process.platform === 'win32';
+  // needed for PNPM
   if (isWindows) {
-    // TODO: test on windows
+    const nodeModulesDir = join(directory, 'node_modules');
+    const files = readdirSync(nodeModulesDir, { recursive: true, withFileTypes: true });
+    if (files.some((f) => f.isSymbolicLink())) {
+      // Compress-Archive doesn't support resolving symlinks like `zip` command
+      // does with -y option. Therefore, we need to resolve symlinks then compress.
+      resolveSymlinks(files);
+    }
     execSync(`Compress-Archive -Path '${directory}\\*' -DestinationPath '${zipFilePath}' -CompressionLevel Optimal`, {
       stdio: 'inherit',
       shell: 'powershell.exe',
@@ -40,11 +46,27 @@ export function createArchive({ directory, zipFileName, fileGlob = '.', quiet }:
     });
   }
   // check output
-  if (!fs.existsSync(zipFilePath)) {
+  if (!existsSync(zipFilePath)) {
     throw new Error(
       `There was a problem generating the archive for ${directory}; the archive is missing in ${zipFilePath}.`
     );
   }
 
   return zipFilePath;
+}
+
+/**
+ * Resolve symlinks
+ */
+function resolveSymlinks(files: Dirent[]) {
+  for (const file of files) {
+    if (file.isSymbolicLink()) {
+      const tmpDir = mkdtempSync(join(tmpdir(), `${file.name}-`));
+      // cannot do `cpSync` to resolve symlinks in-place so must use temp dir
+      const path = join(file.path, file.name);
+      cpSync(path, tmpDir, { recursive: true, dereference: true, force: true });
+      rmSync(path, { recursive: true });
+      renameSync(tmpDir, path);
+    }
+  }
 }
